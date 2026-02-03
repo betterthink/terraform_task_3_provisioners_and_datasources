5667d797c93ff9bdc798040accf6d0e61315b5e9
betterthink <mykyta.kovalenko0804@gmail.com>
2026-02-03 15:52:59 +0100

    fix the issue

--- network.tf
+++ network.tf
@@ -1,10 +1,3 @@
-resource "azurerm_public_ip" "example" {
-  name                = "${var.prefix}-public-ip"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
 resource "azurerm_network_security_rule" "ssh" {
   name                        = "allow-ssh"
   priority                    = 100
@@ -18,32 +11,20 @@ resource "azurerm_network_security_rule" "ssh" {
   resource_group_name         = data.azurerm_resource_group.example.name
   network_security_group_name = azurerm_network_security_group.example.name
 }
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+resource "azurerm_network_security_rule" "http" {
+  name                        = "allow-http"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
 resource "azurerm_network_security_group" "example" {
   name                = "${var.prefix}-nsg"
   location            = data.azurerm_resource_group.example.location
--- vm.tf
+++ vm.tf
@@ -1,55 +1,21 @@
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.example.location
-  resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B1s"
+resource "null_resource" "setup_nginx" {
+  triggers = {
+    vm_id = data.azurerm_virtual_machine.main.id
+  }
 
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
+  connection {
+    type     = "ssh"
+    user     = "testadmin"
+    password = "Password1234!"
+    host     = data.azurerm_public_ip.example.ip_address
   }
 
   provisioner "file" {
     source      = "${path.module}/index.html"
     destination = "/home/testadmin/index.html"
-
-    connection {
-      type     = "ssh"
-      user     = "testadmin"
-      password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
-    }
   }
 
   provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "testadmin"
-      password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
-    }
-
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get install -y nginx",
